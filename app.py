337a5847622bfba2fa8524bc4c48e7bc4cc1647a
Swetha S <sherlinaswetha2702@gmail.com>
2025-10-02 02:27:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,6 @@ def predict():
         return render_template('index.html', prediction_text=f'Error: {str(e)}')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
